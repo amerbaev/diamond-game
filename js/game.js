3067d8507256dd14957094f2bd8f81fa7df9f451
Arseny Amerbaev <amerbaev@gmail.com>
2022-12-10 18:32:07 +0300

    player changing

--- js/game.js
+++ js/game.js
@@ -9,18 +9,47 @@ window.onload = function () {
 
     var longestLineCells = 9;
 
-    var canvasSize = 500;
+    var canvasSize = 800;
     var sectionSize = canvasSize / longestLineCells;
     canvas.width = canvasSize;
     canvas.height = canvasSize;
     // context.translate(0.5, 0.5);
 
+    function drawCellsInRhomb () {
+        context.strokeStyle = "#ddd";
+        context.lineWidth = 5;
+        context.beginPath();
+
+        for (var x = 1; x < longestLineCells; x++) {
+            var upperFrom = sectionSize * 4; 
+            if (x < longestLineCells / 2) {
+                var left = upperFrom - sectionSize * (x-1);
+                var right = upperFrom + sectionSize * x;
+            } else {
+                var left = upperFrom - sectionSize * (longestLineCells-x-1);
+                var right = upperFrom + sectionSize * (longestLineCells - x);
+            }
+            
+            context.moveTo(left, sectionSize * x);
+            context.lineTo(right, sectionSize * x);
+
+            context.moveTo(sectionSize * x, left);
+            context.lineTo(sectionSize * x, right);
+        }
+
+        context.stroke();
+    }
+
+    drawCellsInRhomb();
+
     function drawRhomb () {
         context.strokeStyle = "#000";
-        context.lineWidth = 3;
+        context.lineWidth = 5;
+        context.lineCap = "round";
         context.beginPath();
 
         for (var x = 0; x <= longestLineCells; x++) {
+            
             var upperFrom = sectionSize * 4; 
             if (x < longestLineCells / 2) {
                 var fromLeft = upperFrom - sectionSize * (x-1);
@@ -47,10 +76,61 @@ window.onload = function () {
 
             context.moveTo(toRight, sectionSize * x);
             context.lineTo(toRight, sectionSize * (x+1));
+            
         }
 
         context.stroke();
     }
 
     drawRhomb();
+
+    function drawO(xCordinate, yCordinate) {
+        var halfSectionSize = (0.5 * sectionSize);
+        var centerX = xCordinate + halfSectionSize;
+        var centerY = yCordinate + halfSectionSize;
+        var radius = (sectionSize - halfSectionSize) * 0.8;
+        var startAngle = 0 * Math.PI;
+        var endAngle = 2 * Math.PI;
+
+        context.lineWidth = 5;
+        context.strokeStyle = "#01bBC2";
+        context.beginPath();
+        context.arc(centerX, centerY, radius, startAngle, endAngle);
+        context.stroke();
+    }
+
+    function drawX(xCordinate, yCordinate) {
+        context.strokeStyle = "#f1be32";
+        context.lineWidth = 5;
+        context.beginPath();
+
+        // var offset = (0.5 * sectionSize);
+        var offset = sectionSize / 8;
+        context.moveTo(xCordinate + offset, yCordinate + offset);
+        context.lineTo(xCordinate + sectionSize - offset, yCordinate + sectionSize - offset);
+
+        context.moveTo(xCordinate + offset, yCordinate + sectionSize - offset);
+        context.lineTo(xCordinate + sectionSize - offset, yCordinate + offset);
+
+        context.stroke();
+    }
+
+    var isXPlayer = true;
+    var playerColor = "#f1be32";
+    drawX(0, 0);
+    
+    function changePlayer() {
+        isXPlayer = !isXPlayer;
+        playerColor = isXPlayer ? "#f1be32" : "#01bBC2";
+        context.clearRect(0, 0, sectionSize, sectionSize);
+        if (isXPlayer) {
+            drawX(0, 0);
+        } else {
+            drawO(0, 0);
+        }
+    }
+
+    onmouseup = function (e) {
+        changePlayer();
+    }
 };
